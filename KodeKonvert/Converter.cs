71ddf2ce17a44f90e80123713ec8528d72d2fdfa
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-01-28 10:10:47 +0100

    initialize converter class

---
+++ KodeKonvert/Converter.cs
@@ -0,0 +1,25 @@
+﻿namespace KodeKonvert
+{
+    public class Converter
+    {
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public double KelvinToCelsius(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+
+        public double CelsiusToKelvin(double celsius)
+        {
+            return celsius + 273.15;
+        }
+    }
+}
